b411ce887719561927cef5e6d013cb67d86af40d
Fred Almeida <fredericonascimento97@gmail.com>
2020-11-09 10:33:50 -0300

    Ajustando configuracoes

--- src/main/java/br/com/wod/quartz/config/SchedulerConfig.java
+++ src/main/java/br/com/wod/quartz/config/SchedulerConfig.java
@@ -1,13 +1,11 @@
 package br.com.wod.quartz.config;
 
 import br.com.wod.quartz.jobs.MyJob;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
+import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 
 @Configuration
 public class SchedulerConfig {
@@ -20,6 +18,17 @@ public class SchedulerConfig {
 
     @Bean
     public JobDetail enelJob() {
-        return JobBuilder.newJob(MyJob.class).withIdentity("FirstJob", "group1").build();
+        return JobBuilder.newJob(MyJob.class)
+                .withIdentity("FirstJob", "group1").build();
+    }
+
+    @Bean
+    public Trigger simpleSchedule() {
+        return TriggerBuilder.newTrigger().withIdentity("MyTrigger", "group1")
+                .startNow()
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .repeatForever()
+                        .withIntervalInSeconds(3))
+                .build();
     }
 }
